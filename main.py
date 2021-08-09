742ebed849ed257c82a2b835c86f5266784fa20f
Purple Jaguar <74114303+PurpleJaguar1@users.noreply.github.com>
2021-08-09 14:46:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
-gdd.download_file_from_google_drive(file_id='13NtBIdkP1yx8jGKmYPoh02nGlTjjE4_o',
-                                    dest_path='./test/',
-                                    unzip=True)
+gdd.download_file_from_google_drive(file_id='1KnunRabg1WTp84P5zmJAL73ctTtwnCAh', dest_path='C:/Users/jackd/OneDrive/Documents/GitHub/test/AMP_A321.zip', unzip=True)
