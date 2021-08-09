ee98de6055cc975b4f2738551ba4bd83ea2f0989
Purple Jaguar <74114303+PurpleJaguar1@users.noreply.github.com>
2021-08-09 14:25:09 -0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='13NtBIdkP1yx8jGKmYPoh02nGlTjjE4_o',
+                                    dest_path='./test/',
+                                    unzip=True)
